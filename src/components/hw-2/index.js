22f365d515be14c2c0f73e564a268312ed12cd22
Anna Barsukova <Anna Barsukova>
2022-10-02 20:34:31 +0300

    add hw-2

---
+++ src/components/hw-2/index.js
@@ -0,0 +1,12 @@
+let arr = [ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ], 1, 8];
+
+let sum = 0;
+
+function sumTree(array){
+    array.forEach((item) =>{
+        Array.isArray(item) ? sumTree(item) : sum +=item
+    });
+    return sum;
+}
+console.log(sumTree(arr));
+
