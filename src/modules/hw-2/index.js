1cd30cf035465412e2cb690dff571e5aff8137fe
Anna <test@mail.ru>
2022-10-03 15:38:06 +0300

    add the correction of  hw-2

--- src/modules/hw-2/index.js
+++ src/modules/hw-2/index.js
@@ -1,10 +1,13 @@
+//сумма всех элементов массива 
+
 let arr = [ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ], 1, 8];
 
-let sum = 0;
 
 function sumTree(array){
+    let sum = 0;
     array.forEach((item) =>{
-        Array.isArray(item) ? sumTree(item) : sum +=item;
+        sum += Array.isArray(item) ?
+        sumTree(item) : item;
     });
     return sum;
 }
